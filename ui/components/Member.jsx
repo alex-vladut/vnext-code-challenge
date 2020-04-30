7b77b4004da1069bb774f1cfbe5f9031b0b3e29b
Alex Vladut <alexandruvladut@gmail.com>
2020-04-30 19:19:18 +0300

    feat(app): show checkout button after 5 seconds

--- ui/components/Member.jsx
+++ ui/components/Member.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import { format } from 'date-fns';
+import React, { useState, useEffect } from 'react';
+import { format, differenceInSeconds } from 'date-fns';
 
 const isCheckedIn = member => !!member.checkedInAt;
 const isCheckedOut = member => !!member.checkedOutAt;
+
+const shouldShowCheckOut = member => {
+  return (
+    isCheckedIn(member) &&
+    !isCheckedOut(member) &&
+    differenceInSeconds(new Date(), member.checkedInAt) > 5
+  );
+};
+
 const formatDate = date => {
   if (!date) {
     return 'N/A';
@@ -11,6 +20,25 @@ const formatDate = date => {
 };
 
 export const Member = ({ member, onCheckIn, onCheckOut }) => {
+  const [checkedIn, setCheckedIn] = useState(isCheckedIn(member));
+  const [showCheckOut, setShowCheckOut] = useState(shouldShowCheckOut(member));
+
+  const handleCheckIn = () => {
+    setCheckedIn(true);
+    onCheckIn(member);
+  };
+  const handleCheckOut = () => {
+    setShowCheckOut(false);
+    onCheckOut(member);
+  };
+  useEffect(() => {
+    if (checkedIn && !isCheckedOut(member)) {
+      const difference = differenceInSeconds(new Date(), member.checkedInAt);
+      if (difference < 5) {
+        setTimeout(() => setShowCheckOut(true), (5 - difference) * 1000);
+      }
+    }
+  }, [checkedIn, member, setShowCheckOut]);
   return (
     <li>
       <div>Name: {`${member.firstName} ${member.lastName}`}</div>
@@ -18,13 +46,13 @@ export const Member = ({ member, onCheckIn, onCheckOut }) => {
       <div>Title: {member.title || 'N/A'}</div>
       <div>Check-in date: {formatDate(member.checkedInAt)}</div>
       <div>Check-out date: {formatDate(member.checkedOutAt)}</div>
-      {!isCheckedIn(member) ? (
-        <button onClick={onCheckIn}>
+      {!checkedIn ? (
+        <button onClick={handleCheckIn}>
           Check-in {`${member.firstName} ${member.lastName}`}
         </button>
       ) : null}
-      {isCheckedIn(member) && !isCheckedOut(member) ? (
-        <button onClick={onCheckOut}>
+      {showCheckOut ? (
+        <button onClick={handleCheckOut}>
           Check-out {`${member.firstName} ${member.lastName}`}
         </button>
       ) : null}
